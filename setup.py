3d5847a8acf33fca1282b4514452e6ae29d55913
Bryan Brzycki <bbrzycki@berkeley.edu>
2023-01-31 13:20:24 -0800

    Set min pandas version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
        'galpy>=1.7.2',
        'matplotlib>=3.1.3',
        'seaborn>=0.11.2',
-       'pandas',
+       'pandas>=1.3.5',
        'tqdm>=4.47.0',
        'sphinx-rtd-theme>=0.4.3'
     ],
